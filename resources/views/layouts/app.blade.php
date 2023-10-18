47636d113f2c53b48e106c8ff273a58257c873e6
alberorana@gmail.com <alberorana@gmail.com>
2023-10-18 09:14:20 +0900

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,9 +14,10 @@
         <script src="https://cdn.tailwindcss.com"></script>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="css/color.css">
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body class="font-sans antialiased background-color">
+        <div class="min-h-screen">
             <!-- ここに共通ヘッダーのコンポーネントを読み込む -->
             @include('layouts.announce-header')
             @include('layouts.global-header')
@@ -35,5 +36,6 @@
                 {{ $slot }}
             </main>
         </div>
+        @include('layouts.footer')
     </body>
 </html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,30 @@
+<div class="bg-gray-800 text-white py-4">
+    <div class="container mx-auto">
+        <div class="grid grid-cols-3 gap-8">
+            
+            <div>
+                <h3 class="font-semibold text-lg mb-2">レシピを探す</h3>
+                <ul>
+                    <li class="mb-1"><a href="#" class="hover:underline">全てのレシピ</a></li>
+                    <li><a href="#" class="hover:underline">人気のレシピ</a></li>
+                </ul>
+            </div>
+
+            <div>
+                <h3 class="font-semibold text-lg mb-2">レシピを投稿</h3>
+                <ul>
+                    <li><a href="#" class="hover:underline">レシピの投稿</a></li>
+                </ul>
+            </div>
+
+            <div>
+                <h3 class="font-semibold text-lg mb-2">お知らせのページ</h3>
+                <ul>
+                    <li class="mb-1"><a href="#" class="hover:underline">ニュース一覧 (新窓)</a></li>
+                    <li><a href="#" class="hover:underline">ログイン</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</div>
