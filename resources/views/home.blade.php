5f963c48e429af16be3976cf9e473df46318e40d
alberorana@gmail.com <alberorana@gmail.com>
2023-10-19 10:36:15 +0900

    cardok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,34 @@
       </ul>
     </div>
     <div class="col-span-2 bg-white rounded p-4">
-      2
+      <h2 class="text-2xl font-bold mb-2">新着レシピ</h2>
+  @foreach($recipes as $recipe)
+      <a href="" class="flex flex-col items-center bg-white mb-6 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+        <img class="object-cover rounded-t-lg h-40 w-40 mrounded-none rounded-l-lg" src="{{$recipe->image}}" alt="{{$recipe->title}}">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-800">{{$recipe->title}}</h5>
+          <p class="mb-3 font-normal">{{ $recipe->description }}</p>
+          <div class="flex">
+            <p class="font-bold mr-2">{{$recipe->name}}</p>
+            <p class="text-gray-500">{{$recipe->created_at->format('Y年m月d日')}}</p>
+          </div>
+        </div>
+      </a>
+  @endforeach
+      <a href="" class="text-gray-600 block text-right">すべてのレシピへ ></a>
+      <h2 class="text-2xl font-bold mb-2">人気レシピ</h2>
+      <div class="flex justify-between items-center mb-6">
+  @foreach($popular as $p)
+        <div class="max-12 rounded overflow-hidden shadow-lg mx-4">
+          <img class="max-h-44 h-44 w-full" src="{{$p->image}}" alt="{{$p->title}}">
+          <div class="px-6 py-4">
+            <div class="font-bold text-large mb-2">{{$p->title}}</div>
+            <p class="text-gray-700 text-base">{{$p->description}}</p>
+          </div>
+        </div>
+  @endforeach
+      </div>
+      <a href="" class="text-gray-600 block text-right">すべての人気レシピへ ></a>
     </div>
     <div class="col-span-1 bg-gray ml-4">
       1
